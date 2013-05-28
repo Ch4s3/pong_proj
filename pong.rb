afa40ed5183fcd6c2af3a4944a66484f8d112414
git-Ch4s3 <git-chase.gilliam@gmail.com>
2013-05-28 12:06:58 -0400

    working on collision

--- pong.rb
+++ pong.rb
@@ -1,5 +1,4 @@
 #1st attempt at rubygame for twist on pong
-
 require 'rubygems'
 require 'rubygame'
 
@@ -13,11 +12,12 @@ class Game
         @clock = Rubygame::Clock.new
         @clock.target_framerate = 60
         
-        @player = Paddle.new 50, 10
-        @enemy = Paddle.new @screen.width-50-@player.width,10
-        
+        limit = @screen.height - 10
+        @player = Paddle.new 50, 10, Rubygame::K_W, Rubygame::K_S, 10, limit
+        @enemy = Paddle.new @screen.width-50-@player.width,10, Rubygame::K_UP, Rubygame::K_DOWN, 10, limit
         @player.center_y @screen.height
         @enemy.center_y @screen.height
+        @ball = Ball.new @screen.width/2 - 16, @screen.height/2 -16
         @background = Background.new @screen.width, @screen.height
     
     end
@@ -32,12 +32,38 @@ class Game
     
     def update
         @queue.each do |ev|
+        @player.handle_event ev
+        @enemy.handle_event ev
+        
+        @player.update
+        @enemy.update
+        @ball.update @screen
+        
+        if collision? @ball, @player
+                @ball.collision @player, @screen
+        elsif collision? @ball, @enemy
+                @ball.collision @enemy, @screen
+        end
+        
             case ev
                 when Rubygame::QuitEvent
                     Rubygame.quit
                     exit
+                when Rubygame::KeyDownEvent
+                        if ev.key == Rubygame::K_ESCAPE
+                                @queue.push Rubygame::QuitEvent.new
+                        end
             end
         end
+        
+        def collision? obj1, obj2
+                if obj1.y + obj1.height < obj2.y ; return false
+                if obj1.y > obj2.y + obj2.height ; return false
+                if obj1.x + obj1.width < obj2.x ; return false
+                if obj1.x > obj2.x + obj2.width ; return false
+                return true
+        end
+        
     end
     
     def draw
@@ -46,6 +72,7 @@ class Game
         @background.draw @screen
         @player.draw @screen 
         @enemy.draw @screen
+        @ball.draw @screen
         
         @screen.flip
     end
@@ -60,8 +87,7 @@ class GameObject
         @surface = surface
         @width = surface.width
         @height = surface.height
-
-        
+              
     end
     
     def update
@@ -76,14 +102,71 @@ class GameObject
 end
 
 class Paddle < GameObject
-        def initialize x, y
+        def initialize x, y, up_key, down_key, top_limit, bottom_limit
                 surface = Rubygame::Surface.new [20, 100]
-                surface.fill [0,0,0]
+                surface.fill [255,255,255]
+                @up_key = up_key
+                @down_key = down_key
+                @moving_up = false
+                @moving_down = false
+                @top_limit = top_limit
+                @bottom_limit = bottom_limit
                 super x, y, surface
         end
         
         def center_y h
                 @y = h/2-@height/2
+        end
+        
+        
+        def handle_event event
+                case event
+                        when Rubygame::KeyDownEvent
+                                if event.key == @up_key
+                                        @moving_up = true
+                                elsif event.key == @down_key
+                                        @moving_down = true
+                                end             
+                        when Rubygame::KeyUpEvent
+                                if event.key == @up_key
+                                        @moving_up = false
+                                elsif event.key == @down_key
+                                        @moving_down = false
+                                end
+                        end
+        end
+
+        def update
+                if @moving_up and @y > @top_limit
+                        @y -= 10
+                end
+                if @moving_down and @y+@height < @bottom_limit
+                        @y += 10
+                end
+        end
+        
+end
+
+class Ball < GameObject
+        def initialize x, y
+                surface = Rubygame::Surface.load "ruby.png"
+                @vx = @vy = 5
+                super x, y, surface
+        end
+        
+        def update screen
+                @x += @vx
+                @y += @vy
+                
+                #left / right
+                if @x <= 10 or @x+@width >= screen.width-10
+                        @vx *= -1
+                end
+                
+                #top / bottom
+                if @y <= 10 or @y+@height >= screen.height-10
+                        @vy *= -1
+                end
         end
 end
 
@@ -94,6 +177,7 @@ class Background < GameObject
         # Draw background
         white = [255, 255, 255]
         
+
         #middle circle
         surface.draw_circle_s [surface.width/2,surface.height/2], 100, white # [surface, center, radius, color]
         
